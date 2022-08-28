56a9df69543b13561cd9644cdfe561d798321f45
Simon Vargas <sjvargas01@gmail.com>
2022-08-27 21:43:26 -0400

    test commit

--- src/main/java/com/sha/springbootmicroservice3apigateway/SpringBootMicroservice3ApiGatewayApplication.java
+++ src/main/java/com/sha/springbootmicroservice3apigateway/SpringBootMicroservice3ApiGatewayApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootMicroservice3ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootMicroservice3ApiGatewayApplication.class, args);
+        SpringApplication.run(
+                SpringBootMicroservice3ApiGatewayApplication.class, args);
     }
 
 }
