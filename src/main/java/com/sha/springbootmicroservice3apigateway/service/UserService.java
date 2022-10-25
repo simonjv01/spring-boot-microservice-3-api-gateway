abb1bb414d558a7959dba64027c8bf2249badeee
Simon Vargas <sjvargas01@gmail.com>
2022-10-24 20:32:39 -0400

    updated userserviceimpl and application properties

--- src/main/java/com/sha/springbootmicroservice3apigateway/service/UserService.java
+++ src/main/java/com/sha/springbootmicroservice3apigateway/service/UserService.java
@@ -1,8 +1,15 @@
 package com.sha.springbootmicroservice3apigateway.service;
 
+import com.sha.springbootmicroservice3apigateway.model.Role;
 import com.sha.springbootmicroservice3apigateway.model.User;
+
+import java.util.Optional;
 
 public interface UserService {
 
     User saveUser(User user);
+
+    Optional<User> findByUsername(String username);
+
+    void changeRole(Role newRole, String username);
 }
--- src/main/java/com/sha/springbootmicroservice3apigateway/service/UserServiceImpl.java
+++ src/main/java/com/sha/springbootmicroservice3apigateway/service/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -27,4 +28,16 @@ public class UserServiceImpl implements UserService {
 
         return userRepository.save(user);
     }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public void changeRole(Role newRole, String username) {
+        userRepository.updateUserRole(username, newRole);
+    }
+
+
 }
