2130bc81aa73e18b83b4455ad3c45172ad7a4435
simonjv01 <sjvargas01@gmail.com>
2022-11-03 23:00:38 -0400

    updates to jwtproviderimpl

--- src/main/java/com/sha/springbootmicroservice3apigateway/security/jwt/JwtProviderImpl.java
+++ src/main/java/com/sha/springbootmicroservice3apigateway/security/jwt/JwtProviderImpl.java
@@ -1,7 +1,14 @@
 package com.sha.springbootmicroservice3apigateway.security.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JwtProviderImpl implements JwtProvider {
+public class JwtProviderImpl implements JwtProvider
+{
+    @Value("${app.jwt.secret")
+    private String JWT_SECRET;
+
+    @Value("${app.jwt.expiration-in-ms}")
+    private String JWT_EXPIRATION_IN_MS;
 }
