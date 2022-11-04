246eb6a41c6fc236aa2a6f2dd49677dda79c8ead
simonjv01 <sjvargas01@gmail.com>
2022-11-03 23:10:23 -0400

    added sha encryption

--- src/main/java/com/sha/springbootmicroservice3apigateway/security/jwt/JwtProviderImpl.java
+++ src/main/java/com/sha/springbootmicroservice3apigateway/security/jwt/JwtProviderImpl.java
@@ -1,7 +1,17 @@
 package com.sha.springbootmicroservice3apigateway.security.jwt;
 
+import com.sha.springbootmicroservice3apigateway.security.UserPrincipal;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProviderImpl implements JwtProvider
@@ -11,4 +21,21 @@ public class JwtProviderImpl implements JwtProvider
 
     @Value("${app.jwt.expiration-in-ms}")
     private String JWT_EXPIRATION_IN_MS;
+
+    public String generateToken(UserPrincipal auth)
+    {
+        String authorities = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .setSubject(auth.getUsername())
+                .claim("roles", authorities)
+                .claim("userId", auth.getId())
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_IN_MS))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
 }
