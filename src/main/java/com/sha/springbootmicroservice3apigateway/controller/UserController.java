c5347cd7d586393e007c321873a3190ac4f76056
Simon Vargas <sjvargas01@gmail.com>
2022-12-21 21:12:26 -0500

    created user controller

---
+++ src/main/java/com/sha/springbootmicroservice3apigateway/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.sha.springbootmicroservice3apigateway.controller;
+
+import com.sha.springbootmicroservice3apigateway.model.Role;
+import com.sha.springbootmicroservice3apigateway.security.UserPrincipal;
+import com.sha.springbootmicroservice3apigateway.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")//pre-path
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    public ResponseEntity<?> changeRole(@AuthenticationPrincipal UserPrincipal userPrincipal, @PathVariable Role role)
+    {
+
+        return null;
+    }
+
+}
