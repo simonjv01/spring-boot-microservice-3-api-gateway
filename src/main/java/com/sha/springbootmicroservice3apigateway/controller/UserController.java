860b73fc1effad88ed184822b0aa978537327309
Simon Vargas <sjvargas01@gmail.com>
2022-12-26 08:38:37 -0800

    updated user controller

--- src/main/java/com/sha/springbootmicroservice3apigateway/controller/UserController.java
+++ src/main/java/com/sha/springbootmicroservice3apigateway/controller/UserController.java
@@ -7,16 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/user")//pre-path
-public class UserController {
+public class UserController
+{
 
     @Autowired
     private UserService userService;
 
+    @PutMapping("change/{role}")//api/user/change/{role}
     public ResponseEntity<?> changeRole(@AuthenticationPrincipal UserPrincipal userPrincipal, @PathVariable Role role)
     {
 
